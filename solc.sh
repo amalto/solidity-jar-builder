88cda53b3aa4ff12cf7c432e3d7961cc87db6f62
Choucri FAHED <choucri.fahed@mines-nancy.org>
2018-12-06 11:18:40 +0100

    Enhance comments

--- solc.sh
+++ solc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Installing solc
-#brew tap ethereum/ethereum
-#brew install solidity
-#which solc
+# Install solc on Mac with Homebrew:
+# > brew tap ethereum/ethereum
+# > brew install solidity
+
+# Or with NPM:
+# > npm install -g solc
 
 solc ./src/main/resources/SimpleStorage.sol --bin --abi --optimize -o ./target
-
-
